338514f818a24a861228e98507b992d4e6ce344e
SinBin10 <bincode10@gmail.com>
2024-05-31 22:34:17 +0530

    core react - end of React.createElement("h1",{},"i am an h1 tag").

--- App.js
+++ App.js
@@ -1,9 +1,14 @@
 /*
+This is the core of react meaning that react was originally made to be usd this way but the nested structures of componenents you see made the code a bit too complex to understand so this approach was discarded
+
 <div id="parent">
     <div id="child">
     <h1>
         I am h1 tag
     </h1>
+    <h2>
+        i am h2 tag
+    </h2>
     <div>
 </div
 */
@@ -11,11 +16,10 @@
 const parent = React.createElement(
   "div",
   { id: "parent" },
-  React.createElement(
-    "div",
-    { id: "child" },
-    React.createElement("h1", { id: "heading" }, "I am h1 tag")
-  )
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", { id: "heading" }, "I am h1 tag"),
+    React.createElement("h2", { id: "heading2" }, "I am h2 tag"),
+  ])
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
