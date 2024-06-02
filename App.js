03c0154092bad32a484ce847ec9ff3d6fdc18666
SinBin10 <bincode10@gmail.com>
2024-06-02 22:33:22 +0530

    notes on elements and components.

--- App.js
+++ App.js
@@ -1,21 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 /*
-This is the core of react meaning that react was originally made to be usd this way but the nested structures of componenents you see made the code a bit too complex to understand so this approach was discarded
+This is the core of react meaning that react was originally made to be usd this way but the nested structures of components you see made the code a bit too complex to understand so this approach was discarded
 */
 
-const heading = React.createElement("h1", { id: "heading" }, "Binay pratap singh");
+//react element
+//no need for return keyword and parenthesis while returning a single JSX element
+//const Title = ()=><h1 id="heading">binay element using JSX</h1>
+//wrap this inside round brackets when returning mulitple components as well
+// const Title = ()=>(<><h1 id="heading">binay element using JSX</h1>
+// <h2>Heading 2 tag</h2></>);
+
+//when using the return keyword you wrap inside curly brackets as it represents a function
+
+//react componenent -> a function which returns JSX
+const Heading = ()=>(
+  <div id="container">
+    <Title />
+    <h1 id="heading">Binay react component</h1>
+  </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// root.render(heading);
 
-//JSX --> a way to write react which makes react, developer friendly
-const jsxHeading = <h1 id="heading">Binay pratap singh using jsx</h1>
-// its not a valid pure javascript syntax
-//bundlers like parcel and vite converts(tranpiles) jsx to core react element which you see above this react element(a javascript object) further get converted to normal html and js which is understood by the browser.
+root.render(<Heading />);
 
-root.render(jsxHeading);
-
-console.log(heading);
-console.log(jsxHeading);
+console.log(Title);
+console.log(<Heading />);
