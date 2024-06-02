de41a75f39ee500847dd471c7593fff1ad4aa08a
SinBin10 <bincode10@gmail.com>
2024-06-02 21:57:07 +0530

    a intro to JSX

--- App.js
+++ App.js
@@ -2,27 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 /*
 This is the core of react meaning that react was originally made to be usd this way but the nested structures of componenents you see made the code a bit too complex to understand so this approach was discarded
-
-<div id="parent">
-    <div id="child">
-    <h1>
-        I am h1 tag
-    </h1>
-    <h2>
-        i am h2 tag
-    </h2>
-    <div>
-</div
 */
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", { id: "heading" }, "I am h1 tag"),
-    React.createElement("h2", { id: "heading2" }, "I am h2 tag"),
-  ])
-);
+const heading = React.createElement("h1", { id: "heading" }, "Binay pratap singh");
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+
+// root.render(heading);
+
+//JSX --> a way to write react which makes react, developer friendly
+const jsxHeading = <h1 id="heading">Binay pratap singh using jsx</h1>
+// its not a valid pure javascript syntax
+//bundlers like parcel and vite converts(tranpiles) jsx to core react element which you see above this react element(a javascript object) further get converted to normal html and js which is understood by the browser.
+
+root.render(jsxHeading);
+
+console.log(heading);
+console.log(jsxHeading);
