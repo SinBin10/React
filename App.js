e2141eee83623028a9a564988c93eb27d35ee47e
SinBin10 <bincode10@gmail.com>
2024-06-06 16:17:27 +0530

    Done with Navbar

--- App.js
+++ App.js
@@ -1,36 +1,42 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import logo from '/Images/logo.jpg';
+
 /*
-This is the core of react meaning that react was originally made to be usd this way but the nested structures of components you see made the code a bit too complex to understand so this approach was discarded
+*  Header
+    - logo
+    - list 
+  Body
+    - search
+    - Card
+  Footer
+    - contact us
+    - Copyright
+    - Address
 */
 
-//react element
-//no need for return keyword and parenthesis while returning a single JSX element
-//const Title = ()=><h1 id="heading">binay element using JSX</h1>
-//wrap this inside round brackets when returning mulitple components as well
-// const Title = ()=>(<><h1 id="heading">binay element using JSX</h1>
-// <h2>Heading 2 tag</h2></>);
-
-//when using the return keyword you wrap inside curly brackets as it represents a function
-
-//react componenent -> a function which returns JSX
-
-const Title = ()=>{
-  return <h1>React Functional component</h1>
+const List = () => {
+  return (
+    <ul class="list">
+      <li><a href="#">About us</a></li>
+      <li><a href="#">Contact</a></li>
+      <li><a href="#">Profile</a></li>
+      <li><a href="#">Cart</a></li>
+    </ul>
+  );
 }
-const Heading = ()=>(
-  <div id="container">
-    {Title()}
-    <Title />
-    <Title></Title>
-    <h1 id="heading">Binay react component</h1>
-  </div>
-);
+
+const Heading = () => {
+  return (
+    <div className="navbar">
+      <div><img className="logo" src={logo} alt="logo" /></div>
+      <List />
+    </div>
+  );
+}
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 
 root.render(<Heading />);
-
-console.log(Title);
-console.log(<Heading />);
