a0f46bbbb4428c36ec80e0d7470e23c14ea1133c
SinBin10 <bincode10@gmail.com>
2024-06-08 23:08:40 +0530

    Making a live API call and working with its data.

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,21 @@
 import Card from "./Card";
 import resObj from "../utils/mockData";
 //named import
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Body = () => {
     const [resList, setResList] = useState(resObj);
+    useEffect(() => {
+        fetchdata();
+    }, []);
+
+    const fetchdata = async () => {
+        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.51800&lng=88.38320&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+        const json = await data.json();
+        console.log(json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants);
+        setResList(json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants);
+    }
+
     return (
         <div>
             <input
