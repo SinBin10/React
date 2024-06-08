3c82e69036ffe62b8fe2bd448fe7f17ba2754fa3
SinBin10 <bincode10@gmail.com>
2024-06-08 23:13:00 +0530

    applying filter to live data.

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,9 @@
 import Card from "./Card";
-import resObj from "../utils/mockData";
 //named import
 import { useState, useEffect } from "react";
 
 const Body = () => {
-    const [resList, setResList] = useState(resObj);
+    const [resList, setResList] = useState([]);
     useEffect(() => {
         fetchdata();
     }, []);
@@ -12,7 +11,6 @@ const Body = () => {
     const fetchdata = async () => {
         const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.51800&lng=88.38320&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
         const json = await data.json();
-        console.log(json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants);
         setResList(json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants);
     }
 
@@ -24,7 +22,7 @@ const Body = () => {
                 name="search"
                 placeholder="Enter you favourite food..."
             />
-            <button className="btn" onClick={() => { const filteredlist = resObj.filter((restaurant) => { return restaurant.info.avgRating > 4 }); setResList(filteredlist); }}>Filter by rating</button>
+            <button className="btn" onClick={() => { const filteredlist = resList.filter((restaurant) => { return restaurant.info.avgRating > 4.5 }); setResList(filteredlist); }}>Filter by rating</button>
             <div className="card-container">
                 {resList.map((restaurant) => <Card key={restaurant.info.id} resData={restaurant} />)}
             </div>
