646b347df8f12715a63d7c5d226ec5a7d745e4e9
SinBin10 <bincode10@gmail.com>
2024-06-15 23:21:45 +0530

    bypassing cors

--- src/components/Body.js
+++ src/components/Body.js
@@ -13,7 +13,7 @@ const Body = () => {
     }, []);
 
     const fetchdata = async () => {
-        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.51800&lng=88.38320&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+        const data = await fetch("https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.51800&lng=88.38320&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
         const json = await data.json();
         setResList(json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants);
         setCopyList(json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants);
